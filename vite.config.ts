05d7f417daa9bee989e0f6e0e6c127bed8484aff
catnaut <catnaut@outlook.com>
2025-11-16 11:37:58 +0800

    hot fix ssr

--- vite.config.ts
+++ vite.config.ts
@@ -4,10 +4,13 @@ import tsConfigPaths from 'vite-tsconfig-paths';
 import tailwindcss from '@tailwindcss/vite';
 import viteReact from '@vitejs/plugin-react';
 import netlify from '@netlify/vite-plugin-tanstack-start';
+import { nitro } from 'nitro/vite'
+
 
 export default defineConfig({
   server: {},
   plugins: [
+    nitro(),
     tailwindcss(),
     tsConfigPaths({
       projects: ['./tsconfig.json']
